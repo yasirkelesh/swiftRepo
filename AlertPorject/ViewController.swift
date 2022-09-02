b3a521ab6c450c21463b987134851d598f4bfc00
Muhammed yasir Keles <mukeles@k2m08s08.42kocaeli.com.tr>
2022-09-02 13:57:05 +0300

    kdsjg

---
+++ AlertPorject/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AlertPorject
+//
+//  Created by Muhammed yasir Keles on 8/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var UserNameText: UITextField!
+    @IBOutlet weak var PasswordText: UITextField!
+    @IBOutlet weak var PasswordAgainText: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func SignUpButton(_ sender: Any) {
+        
+        let alert = UIAlertAction
+    }
+    
+}
+
